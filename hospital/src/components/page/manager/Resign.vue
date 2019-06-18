b16a34ea5d58f9fdbe811f42e5f715e99141eaa8
Administrator <2972169182@qq.com>
2019-06-18 11:28:08 +0800

    完善路由

---
+++ hospital/src/components/page/manager/Resign.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>人员流动</h1>
+</template>
+
+<script>
+    export default {
+        name: "Resign"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- hospital/src/router.js
+++ hospital/src/router.js
@@ -21,14 +21,14 @@ export const constantRouterMap = [
     meta: { title: '自述文件' },
     children:[
       {
-        path: '/duty',
-        component: () => import('./components/page/manager/Duty'),
-        meta: { title: '值班管理' }
-      },
-      {
         path: '/doctor',
         component: () => import('./components/page/doctor/Doctor'),
         meta: { title: '医生诊断' }
+      },
+      {
+        path: '/duty',
+        component: () => import('./components/page/manager/Duty'),
+        meta: { title: '值班管理' }
       },
       {
         path: '/infoManager',
@@ -36,10 +36,55 @@ export const constantRouterMap = [
         meta: { title: '人员信息管理' }
       },
       {
+        path:'/resign',
+        component:()=> import('./components/page/manager/Resign'),
+        meta: { title: '人员流动' }
+      },
+      {
         path: '/jurisdiction',
         component: () => import('./components/page/jurisdiction/Jurisdiction.vue'),
         meta: { title: '角色管理' }
+      },{
+        path:'/registerVisi',
+        component:()=> import('./components/page/cashier/RegisterVisi'),
+        meta: { title: '挂号预约' }
+      },
+      {
+        path:'/registerInfo',
+        component:()=> import('./components/page/cashier/RegisterInfo'),
+        meta: { title: '挂号查询' }
+      },
+      {
+        path:'/prescript',
+        component:()=> import('./components/page/pharmacy/Prescript'),
+        meta: { title: '处理药方' }
+      },
+      {
+        path:'/inquiries',
+        component:()=> import('./components/page/pharmacy/Inquiries'),
+        meta: { title: '药品查询' }
+      },
+      {
+        path:'/replenishment',
+        component:()=> import('./components/page/pharmacy/Replenishment'),
+        meta: { title: '申请补给' }
+      },
+      {
+        path:'/waste',
+        component:()=> import('./components/page/medicine/Waste'),
+        meta: { title: '浪费药物' }
+      },
+      {
+        path:'/storehouse',
+        component:()=> import('./components/page/medicine/Storehouse'),
+        meta: { title: '药库查询' }
+      },
+      {
+        path:'/delivery',
+        component:()=> import('./components/page/medicine/Delivery'),
+        meta: { title: '药物出药' }
       }
+
     ]
   },
   {
