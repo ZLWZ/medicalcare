b7d68182b64aff1e052e6aaababb93858d01a91b
Administrator <2972169182@qq.com>
2019-06-22 08:19:35 +0800

    个人中心

--- medicalcare-consumer/src/main/java/com/medicalcare/medicalcareconsumer/service/LoginService.java
+++ medicalcare-consumer/src/main/java/com/medicalcare/medicalcareconsumer/service/LoginService.java
@@ -9,13 +9,14 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Map;
 
 @FeignClient("MEDICALCARE-PROVIDE")
 public interface LoginService {
@@ -36,4 +37,20 @@ public interface LoginService {
 
     @RequestMapping(value = "/unauthorized")
     public Result unauthorized(@RequestParam("code") int code, HttpServletRequest request);
+
+    @PostMapping(value = "/verifyPassword")
+    public Result verifyPassword(@RequestBody Map map);
+
+    @PostMapping(value = "/updatePassword")
+    public Result updatePassword(@RequestBody Map map);
+
+
+    @GetMapping(value = "/selCurrentUser/{id}")
+    public Result selUser(@PathVariable(value = "id") String uid);
+
+    @PostMapping(value = "/updatePortrait")
+    public Result updatePortrait(@RequestParam(value = "file",required = false) MultipartFile file, @RequestParam(value = "uid") String uid);
+
+    @PostMapping(value="updateUser")
+    public Result updateUser(@RequestBody User user);
 }
